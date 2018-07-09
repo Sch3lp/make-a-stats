cc374ebb858d8c9e18f93bcdd1fca00ecbaba994
sch3lp <sch3lp@gmail.com>
2018-07-09 11:28:36 +0200

    Fixed compilation in cdc

--- backend_kotlin/test-cdc/src/main/kotlin/be/swsb/makeastats/testcdc/HttpBaseConfig.kt
+++ backend_kotlin/test-cdc/src/main/kotlin/be/swsb/makeastats/testcdc/HttpBaseConfig.kt
@@ -3,7 +3,9 @@ package be.swsb.makeastats.testcdc
 import be.swsb.makeastats.kotlinbackend.domain.leaderboard.*
 import be.swsb.makeastats.kotlinbackend.domain.playerstats.PlayerStats
 import be.swsb.makeastats.kotlinbackend.domain.playerstats.PlayerStatsRepo
+import be.swsb.makeastats.kotlinbackend.pubgacl.acl.PlayerAcl
 import be.swsb.makeastats.kotlinbackend.services.LeaderboardService
+import be.swsb.makeastats.kotlinbackend.services.PlayerStatsService
 import com.nhaarman.mockito_kotlin.mock
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,7 +18,7 @@ open class HttpBaseConfig {
     @Bean
     @Primary
     open fun leaderboardService(): LeaderboardService {
-        return LeaderboardServiceStub(leaderboardRepo(), playerStatsRepo())
+        return LeaderboardServiceStub(leaderboardRepo(), playerStatsRepo(), playerStatsService())
     }
 
     @Bean
@@ -33,14 +35,27 @@ open class HttpBaseConfig {
 
     @Bean
     @Primary
+    open fun playerAcl(): PlayerAcl {
+        return mock()
+    }
+
+    @Bean
+    @Primary
+    open fun playerStatsService(): PlayerStatsService {
+        return mock()
+    }
+
+    @Bean
+    @Primary
     open fun datasource(): DataSource {
         return mock()
     }
 }
 
 class LeaderboardServiceStub(override val leaderboardRepo: LeaderboardRepo,
-                             override val playerStatsRepo: PlayerStatsRepo)
-    : LeaderboardService(leaderboardRepo, playerStatsRepo) {
+                             override val playerStatsRepo: PlayerStatsRepo,
+                             override val playerStatsService: PlayerStatsService)
+    : LeaderboardService(leaderboardRepo, playerStatsRepo, playerStatsService) {
 
     override fun handle(cmd: CreateLeaderBoardCmd): LeaderboardWithPlayers? {
         val leaderboard = Leaderboard(CreateLeaderBoardCmd("shroudSquad", setOf("shrood", "wadu")))
